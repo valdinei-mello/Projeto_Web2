e9baee7cb63c7fddf0733072ffef54f30e15072c
Valdiei de Mello <valdinei.mello.22@gmail.com>
2022-04-19 09:46:22 -0300

    promises

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasServices } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers:[OfertasServices]
+  providers: [OfertasServices]
 })
 export class HomeComponent implements OnInit {
+
+  public ofertas?: Oferta[]
 
   constructor(private ofertasService: OfertasServices) { }
 
   ngOnInit(): void {
-   console.log( this.ofertasService.getOfertas())
+    //this.ofertas = this.ofertasService.getOfertas()
 
+    this.ofertasService.getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas
+      })
+
+    // console.log(this.ofertas)
   }
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,70 @@
-export class OfertasServices{
-    public getOfertas():Array<string>{
-        let ofertas = ["Oferta1", "Oferta2", "Oferta3"]
-        return ofertas
+
+import { Oferta } from "./shared/oferta.model"
+
+export class OfertasServices {
+
+    public ofertas: Array<Oferta> =
+        [
+            {
+                id: 1,
+                categoria: "restaurante",
+                titulo: "Super Burger",
+                descricao_oferta: "RodÃ­zio de Mini-hambÃºrger com opÃ§Ã£o de entrada.",
+                anunciante: "Original Burger",
+                valor: 29.90,
+                destaque: true,
+                imagens: [
+                    { url: "/assets/ofertas/1/img1.jpg" },
+                    { url: "/assets/ofertas/1/img2.jpg" },
+                    { url: "/assets/ofertas/1/img3.jpg" },
+                    { url: "/assets/ofertas/1/img4.jpg" }
+                ]
+            },
+            {
+                id: 2,
+                categoria: "restaurante",
+                titulo: "Cozinha Mexicana",
+                descricao_oferta: "AlmoÃ§o ou Jantar com RodÃ­zio Mexicano delicioso.",
+                anunciante: "Mexicana",
+                valor: 32.90,
+                destaque: true,
+                imagens: [
+                    { url: "/assets/ofertas/2/img1.jpg" },
+                    { url: "/assets/ofertas/2/img2.jpg" },
+                    { url: "/assets/ofertas/2/img3.jpg" },
+                    { url: "/assets/ofertas/2/img4.jpg" }
+                ]
+
+            },
+            {
+                id: 4,
+                categoria: "diversao",
+                titulo: "EstÃ¢ncia das Ã¡guas",
+                descricao_oferta: "DiversÃ£o garantida com piscinas, trilhas e muito mais.",
+                anunciante: "EstÃ¢ncia das Ã¡guas",
+                valor: 31.90,
+                destaque: true,
+                imagens: [
+                    { url: "/assets/ofertas/3/img1.jpg" },
+                    { url: "/assets/ofertas/3/img2.jpg" },
+                    { url: "/assets/ofertas/3/img3.jpg" },
+                    { url: "/assets/ofertas/3/img4.jpg" },
+                    { url: "/assets/ofertas/3/img5.jpg" },
+                    { url: "/assets/ofertas/3/img6.jpg" }
+                ]
+            }
+        ]
+
+    public getOfertas(): Array<Oferta> {
+        return this.ofertas
     }
+
+    
+    public getOfertas2(): Promise<Oferta[]> {
+        return new Promise((resolve, rejects) => {
+            //algum tipo de processamento, que ao finalizar, chama a função resolve ou rejeitar
+            resolve(this.ofertas)
+        })
+    }
+
 }
